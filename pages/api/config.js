55bccc473fc987cc5b423d18fe295a4511154b5a
tahsin112233-png <akash.afnan112233@gmail.com>
2026-04-29 21:14:36 +0600

    Create config.js

---
+++ pages/api/config.js
@@ -0,0 +1,24 @@
+// Real server URLs (hidden from users)
+export const SERVER_URLS = {
+  1: 'http://172.31.100.110',
+  2: 'http://100.64.32.2',
+  3: 'http://172.19.178.180',
+  4: 'http://10.99.99.2',
+  5: 'http://tv.basnetbd.com',
+  6: 'http://172.16.215.142',
+  7: 'http://tv.elaach.com',
+  8: 'https://toffeelive.com'
+};
+
+export const CHANNEL_PATHS = {
+  1: '/sports-live',
+  2: '/cricket-hd',
+  3: '/football',
+  4: '/basketball',
+  5: '/tennis',
+  6: '/hockey',
+  7: '/golf',
+  8: '/volleyball',
+  9: '/boxing',
+  10: '/racing'
+};
